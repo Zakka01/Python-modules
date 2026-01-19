b49e6102714f9b7539890996bf3908515e6644a2
Your Name <your.email@example.com>
2026-01-19 22:42:32 +0100

    first commit

---
+++ module00/ex2/ft_harvest_total.py
@@ -0,0 +1,8 @@
+def ft_harvest_total():
+    i = 1
+    total = 0
+    while i <= 3:
+        harvest = int(input(f"Day {i} harvest: "))
+        total += harvest
+        i += 1
+    print(f">> Total Harvest : {total}")
---
+++ module00/ex5/ft_count_harvest_iterative.py
@@ -0,0 +1,7 @@
+def ft_count_harvest_iterative():
+    i = 1
+    time = int(input("Days until harvest: "))
+    while i <= time:
+        print(f"Day {i}")
+        i += 1
+    print(">> Harvest time!")
