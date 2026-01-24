c25902c4727880a9186a9d5761db971ce99d6001
Zakariae Ahrabare <ahrabare8@gmail.com>
2026-01-24 21:21:02 +0100

    .

--- Python01/ex6/ft_garden_analytics.py
+++ Python01/ex6/ft_garden_analytics.py
@@ -3,19 +3,28 @@ class Plant:
         self.name = name
         self.__height = height
         self.__age = age
+        
+    def get_height(self):
+        return self.__height
+
+    def get_age(self):
+        return self.__age
+
     def grow(self):
         self.__height += 1
-    def Age(self):
+
+    def increase_age(self):
         self.__age += 1
-    def get_info(self):
-        print(f"{self.name}: {self.height}cm, {self.age} days")
+
 
 class FloweringPlant(Plant):
     def __init__(self, name, height, age, color):
         super().__init__(name, height, age)
         self.color = color 
     def bloom(self):
-        print(f"{self.name} is blooming in {self.color} color!")
+        return f"{self.name} is blooming in {self.color} color!"
+   
+
             
 class PrizeFlower(FloweringPlant):
     def __init__(self, name, height, age, color, prize_points):
@@ -23,15 +32,40 @@ class PrizeFlower(FloweringPlant):
         self.prize_points = prize_points
     def show_prize(self):
         print(f"{self.name} has {self.prize_points} prize points!")
-      
-      
+    def get_info(self):
+        return f"{self.name}: {self.get_height()}cm, {self.get_age()} days, {self.color} flowers (blooming), Prize points: {self.prize_points}"
+
+
+class Garden:
+    def __init__(self, owner_name):
+        self.owner_name = owner_name
+        self.plants = []
+        
+    def add_plant(self, plant):
+        self.plants.append(plant)
+        print(f"Added {plant.name} to {self.owner_name}'s garden")
+
+    def grow_all(self):
+        print(f"{self.owner_name} is helping all plants grow...")
+        for plant in self.plants:
+            plant.grow()
+            print(f"{plant.name} grew 1cm")
+    
+    def report(self):
+        print(f"=== {self.owner_name}'s Garden Report ===")
+        print("Plants in garden:")
+        for plant in self.plants:
+            print(f"- {plant.get_info()}")
+
+    
+    
 class GardenManager:
-    def __init__(self, garden):
-        self.gradens = garden
+    def __init__(self):
+        self.gardens = []
 
     def add_garden(self, garden):
         self.gardens.append(garden)
-        print(f"Added {self.name} Tree to {self.gradens}'s garden")
+        print(f"Added a garden for {garden.owner_name}!")
 
     def remove_garden(self, index):
         if 0 <= index < len(self.gardens):
