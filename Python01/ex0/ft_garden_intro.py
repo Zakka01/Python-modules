c3b615264d7500d47357a4b52d6f071b8476199b
Zakariae Ahrabare <ahrabare8@gmail.com>
2026-01-27 18:19:37 +0100

    modify ex00 and 01 on python01

--- Python01/ex0/ft_garden_intro.py
+++ Python01/ex0/ft_garden_intro.py
@@ -1,9 +1,7 @@
-def my_garden(name, height, age):
-    print(f"Plant: {name}")
-    print(f"Height: {height}")
-    print(f"Age: {age}")
-
 if __name__ == "__main__":
-    print("=== Welcome to My Garden ===")
-    my_garden("Rose", "25cm", "30 days")
-    print("=== End of Program ===")
+    name = "Rose"
+    height = 25
+    age = 30
+    print(f"Plant : {name}")
+    print(f"Height : {height}cm")
+    print(f"Age : {age} days")
--- Python01/ex1/ft_garden_data.py
+++ Python01/ex1/ft_garden_data.py
@@ -6,3 +6,11 @@ class Plant:
 
     def print_data(self):
         print(f"{self.name} : {self.height}cm, {self.age} days old")
+
+if __name__ == "__main__":
+    rose = Plant("Rose", 25, 30)
+    rose.print_data()
+    flower = Plant("Sunflower", 80, 45)
+    flower.print_data()
+    tree = Plant("Cactus", 15, 120)
+    tree.print_data()
